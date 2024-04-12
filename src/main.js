e8ba9f26e14b72c9bb6c19757582d2e6e9b19bb0
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-12 12:39:09 -0500

    feat: add first components from PrimeVue

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from "primevue/config";
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
 
 const app = createApp(App);
 
 app.use(PrimeVue);
+app.component('pv-button', Button)
+app.component('pv-input-text', InputText)
 
 app.mount('#app')
